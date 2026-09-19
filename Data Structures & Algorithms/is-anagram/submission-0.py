7a9ac57feb057e8397f590abbd98dd83b2687f63
Varun Sai Raigir <64308368+VarunSai02@users.noreply.github.com>
2026-09-19 15:27:11 -0400

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d1 = {}
+        d2 = {}
+        for i in s:
+            if i in d1:
+                d1[i]+=1
+            else:
+                d1[i]=0
+        
+        for i in t:
+            if i in d2:
+                d2[i]+=1
+            else:
+                d2[i]=0
+
+        if d1==d2:
+            return True
+        else:
+            return False
